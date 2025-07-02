5f31ed6eedbc48578e1884bb724ca81ca159aa89
Sean Drucker <srdrucker@gmail.com>
2025-07-02 06:00:40 -0500

    cleanup

--- branches.sh
+++ branches.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# create new
-for i in $(seq 1 $1); do
+# shellcheck disable=SC2086
+for i in $(seq $1 $2); do
   git branch sys-"$i"/main
   git tag sys-"$i"/start
 done
--- clean.sh
+++ clean.sh
@@ -7,10 +7,3 @@ git ls-remote --tags origin | grep -v '\^{}' | grep 'refs/tags/sys-' | cut -f2 |
 # cleanup local
 git branch --format='%(refname:short)' | grep '^sys-' | xargs git branch -d
 git tag | grep '^sys-' | xargs git tag -d
-
-# seq 1 "$1" | xargs -I{} echo sys-{}/main | xargs git push origin --delete
-# seq 1 1000 | xargs -I{} echo sys-{}/main | xargs git branch
-# seq 1 "$1" | xargs -I{} echo sys-{}/desired | xargs git push origin --delete
-
-# seq 1 "$1" | xargs -I{} echo sys-{}/start | xargs git push origin --delete tag
-# seq 1 "$1" | xargs -I{} echo sys-{}/agent | xargs git push origin --delete tag
