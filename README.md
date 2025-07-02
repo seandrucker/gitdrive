556c058ecee5de9f10b79aaafc921bce8241e1cc
Sean Drucker <srdrucker@gmail.com>
2025-07-01 22:40:52 -0500

    cleanup

--- README.md
+++ README.md
@@ -6,3 +6,38 @@ Base repo
 # creates branches and start tags.
 ./branches.sh
 ```
+
+
+
+## SSH
+Connecting over SSH corrects issues with testing that arise from overloading of the credential provider.
+
+```sh
+ls -al ~/.ssh
+ssh-keygen -t ed25519 -C "your_email@example.com"
+
+# check ssh agent running
+eval "$(ssh-agent -s)"
+```
+
+### Mac OS
+```sh
+# add below 
+nano ~/.ssh/config
+```
+
+If no passphrase:
+```
+Host github.com
+  AddKeysToAgent yes
+  IdentityFile ~/.ssh/id_ed25519
+```
+
+```sh
+ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+```
+
+## Add
+```sh
+pbcopy < ~/.ssh/id_ed25519.pub
+```
--- branches.sh
+++ branches.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# cleanup remotes
-git ls-remote --heads origin | grep 'refs/heads/sys-' | cut -f2 | xargs git push origin --delete
-git ls-remote --tags origin | grep -v '\^{}' | grep 'refs/tags/sys-' | cut -f2 | xargs git push origin --delete
-
-# cleanup local
-git branch --format='%(refname:short)' | grep '^sys-' | xargs git branch -d
-git tag | grep '^sys-' | xargs git tag -d
-
-# seq 1 "$1" | xargs -I{} echo sys-{}/main | xargs git push origin --delete
-# seq 1 1000 | xargs -I{} echo sys-{}/main | xargs git branch
-# seq 1 "$1" | xargs -I{} echo sys-{}/desired | xargs git push origin --delete
-
-# seq 1 "$1" | xargs -I{} echo sys-{}/start | xargs git push origin --delete tag
-# seq 1 "$1" | xargs -I{} echo sys-{}/agent | xargs git push origin --delete tag
-
 # create new
 for i in $(seq 1 $1); do
   git branch sys-"$i"/main
